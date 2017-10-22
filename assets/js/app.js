9271a6b7e2fa242199eab945aa7f849a42f59aa7
SteveCarrasco <casteve@wanadoo.fr>
2017-10-22 15:42:47 +0200

    modifs erreus formulaire

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ app.controller('MonControllerPrincipal', function ($scope) {
     $scope.error = false;
     $scope.ButtonEditPushed = false;
     $scope.errorForm = false;
-    $scope.formIsSubmitted = false;
+
 
 
     var geocoder;
@@ -27,7 +27,7 @@ app.controller('MonControllerPrincipal', function ($scope) {
 
     // Add an Element in todolist
     $scope.addElementTodolist = function () {
-        $scope.formIsSubmitted = true;
+
         if ($scope.myForm.$invalid) {
 
             return;
